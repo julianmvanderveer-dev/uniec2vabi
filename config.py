1948bb86751410d9d2b98d5fd917d372b7db2532
Julian van der Veer | Borgch <julian@borgch.nl>
2026-04-01 15:42:23 +0200

    Bedrijfsgegevens Borgch B.V. ingevuld

--- config.py
+++ config.py
@@ -8,15 +8,15 @@ VAT_RATE           = 0.21    # BTW-tarief (21%)
 FREE_UP_TO         = 1       # Bestanden met max. dit aantal woningen zijn gratis
 
 # Bedrijfsgegevens (voor facturen)
-BEDRIJF_NAAM    = 'Borgch B.V.'
+BEDRIJF_NAAM        = 'Borgch B.V.'
 BEDRIJF_HANDELSNAAM = 'Brynt.nl'
-BEDRIJF_ADRES   = ''          # ← vul in, bijv. 'Voorbeeldstraat 1'
-BEDRIJF_POSTCODE = ''         # ← vul in, bijv. '1234 AB'
-BEDRIJF_PLAATS  = ''          # ← vul in, bijv. 'Amsterdam'
-BEDRIJF_KVK     = ''          # ← vul in
-BEDRIJF_BTW     = ''          # ← vul in, bijv. 'NL123456789B01'
-BEDRIJF_IBAN    = ''          # ← vul in, bijv. 'NL00INGB0000000000'
-BEDRIJF_EMAIL   = 'info@borgch.nl'
+BEDRIJF_ADRES       = 'Oranjelaan 3G1'
+BEDRIJF_POSTCODE    = '3311 DH'
+BEDRIJF_PLAATS      = 'Dordrecht'
+BEDRIJF_KVK         = '81091516'
+BEDRIJF_BTW         = 'NL861924824B01'
+BEDRIJF_IBAN        = 'NL23 INGB 0005 9885 80'
+BEDRIJF_EMAIL       = 'info@borgch.nl'
 
 # Admin (factuuroverzicht)
-ADMIN_KEY = 'verander-dit-naar-iets-geheims'  # wachtwoord voor /admin
+ADMIN_KEY = 'Oranjelaan3G!'  # wachtwoord voor /admin
